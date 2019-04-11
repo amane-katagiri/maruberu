1aa22677533112bb07d650dd7ca7ddc76590bf40
Amane Katagiri <amane@ama.ne.jp>
2019-04-11 15:06:34 +0900

    Fix date handling

--- maruberu/models.py
+++ maruberu/models.py
@@ -96,8 +96,10 @@ class BellResource(object):
         self.sticky: bool = sticky
         self.api: bool = api
         self._status: BellResourceStatus = status
-        self.created_at: datetime = datetime.fromisoformat(created_at) or datetime.now(pytz.utc)
-        self.updated_at: datetime = datetime.fromisoformat(updated_at) or datetime.now(pytz.utc)
+        self.created_at: datetime = (datetime.fromisoformat(created_at) if created_at else
+                                     datetime.now(pytz.utc))
+        self.updated_at: datetime = (datetime.fromisoformat(updated_at) if updated_at else
+                                     datetime.now(pytz.utc))
         # not on table
         self._is_before_period: Optional[bool] = None
         self._is_after_period: Optional[bool] = None
